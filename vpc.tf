00e2cf1745d2b3a5c8999e29343f55578ae9a0c5
Disney Cheng <zerofai@gmail.com>
2022-12-02 09:20:28 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -3,14 +3,34 @@ module "vpc" {
     version = "~> 3.0"
 
     project_id   = var.project
-    network_name = "guarav-network"
+    network_name = "gaurav-vpc"
+    routing_mode = "GLOBAL"
 
     subnets = [
-        {
-            subnet_name           = "gaurav-subnet"
-            subnet_ip             = "10.10.10.0/24"
-            subnet_region         = var.region
-        },
+    {
+        subnet_name   = "gaurav-subnet"
+        subnet_ip     = "10.100.10.0/24"
+        subnet_region = var.region
+    }
     ]
 
+
+
+    routes = [
+        {
+            name                   = "egress-internet"
+            description            = "route through IGW to access internet"
+            destination_range      = "0.0.0.0/0"
+            tags                   = "egress-inet"
+            next_hop_internet      = "true"
+        },
+        {
+            name                   = "app-proxy"
+            description            = "route through proxy to reach app"
+            destination_range      = "10.50.10.0/24"
+            tags                   = "app-proxy"
+            next_hop_instance      = "app-proxy-instance"
+            next_hop_instance_zone = "us-west1-a"
+        },
+    ]
 }
