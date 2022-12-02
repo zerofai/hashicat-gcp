fee37f9f6360e03c347efacbe2c147988d0da91e
Disney Cheng <zerofai@gmail.com>
2022-12-02 08:43:29 +0000

    Added the first label

--- main.tf
+++ main.tf
@@ -47,7 +47,6 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
-  deparment    = "devops"
 
   boot_disk {
     initialize_params {
@@ -69,6 +68,7 @@ resource "google_compute_instance" "hashicat" {
 
   labels = {
     name = "hashicat"
+    department = "devops"
   }
 
 }
