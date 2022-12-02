6993c9f27ad3ea3fdf6820a7e23bd08eca24cd65
Disney Cheng <zerofai@gmail.com>
2022-12-02 09:14:53 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,14 +2,14 @@ module "vpc" {
     source  = "terraform-google-modules/network/google"
     version = "~> 3.0"
 
-    project_id   = "p-ssyt7uqehfcb-0"
+    project_id   = var.project
     network_name = "guarav-network"
 
     subnets = [
         {
             subnet_name           = "gaurav-subnet"
             subnet_ip             = "10.10.10.0/24"
-            subnet_region         = "var.region"
+            subnet_region         = var.region
         },
     ]
 
